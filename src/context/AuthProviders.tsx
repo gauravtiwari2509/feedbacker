2cc055dceadcaa52d535d9ac5dbd0579c6521855
gauravtiwari2509 <gauravtiwari2509@gmail.com>
2024-06-19 17:21:56 +0530

    correct missplacing of api folder,and little changes

--- src/context/AuthProviders.tsx
+++ src/context/AuthProviders.tsx
@@ -1,14 +1,15 @@
-'use client'
+'use client';
 
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from 'next-auth/react';
 
 export default function AuthProvider({
   children,
-  
-}:{children:React.ReactNode}) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <SessionProvider >
-    {children}
+    <SessionProvider>
+      {children}
     </SessionProvider>
-  )
+  );
 }
