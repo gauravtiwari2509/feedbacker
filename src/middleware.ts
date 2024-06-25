dc2cf319ee494bfc2b3b69d807679d30fc8f3573
gauravtiwari2509 <gauravtiwari2509@gmail.com>
2024-06-25 20:32:15 +0530

    solve error:- "This page isn’t workinglocalhost redirected you too many times."

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,28 @@
-import { NextResponse, NextRequest } from "next/server";
-// import { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
-// This function can be marked `async` if using `await` inside
+
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
+
+  // Redirect to dashboard if token exists and user accesses sign-in, sign-up, verify, or root paths
   if (
     token &&
     (url.pathname.startsWith("/sign-in") ||
       url.pathname.startsWith("/sign-up") ||
       url.pathname.startsWith("/verify") ||
-      url.pathname.startsWith("/"))
+      url.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+
+  // Redirect to sign-in if no token and user tries to access dashboard
   if (!token && url.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
+
+  // Allow the request to proceed if no redirection conditions are met
   return NextResponse.next();
 }
-
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*"],
-};
